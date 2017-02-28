0a9fcd5d1e951826eb6dd69f06f00ecc9402c32d
mallikachand <mallikamahendrachand@gmail.com>
2017-02-28 14:31:30 +0530

    Update exp.js

--- js/exp.js
+++ js/exp.js
@@ -1,1 +1,28 @@
+var company = new Object();
+company.name = "Facebook";
+company.ceo = new Object();
+company.ceo.firstName = "Mallika";
+company.ceo.favColor = "black";
 
+console.log(company);
+console.log("Company CEO name is: " 
+  + company.ceo.firstName);
+
+console.log(company["name"]);
+var stockPropName = "stock of company";
+company[stockPropName] = 110;
+
+console.log("Stock price is: " + 
+  company[stockPropName]);
+
+// Better way: object literal
+var facebook = {
+  name: "Facebook",
+  ceo: {
+    firstName: "Mallika",
+    favColor: "black"
+  },
+  "stock of company": 110
+};
+
+console.log(facebook.ceo.firstName);
