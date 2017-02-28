3139f20fbe89549f65e9199be8c3d34fddce9145
mallikachand <mallikamahendrachand@gmail.com>
2017-02-28 15:53:43 +0530

    Update exp3.js

--- js/exp3.js
+++ js/exp3.js
@@ -1,12 +1,30 @@
-import java.util.*;
-class HelloWorld
+var a = [2,5,3,1,6,8]
+var x = 4;
+var result;
+function c1(at,x)
 {
-    public static void main(String[] args)
+    var i;
+    var j;
+    var r;
+    var temp;
+    for(i=0,i<(at.length-1); i++)
     {
-        int[] random = {1,3,6,5,2,0,10};
-        System.out.println("Array before sorting : " + Arrays.toString(random));
-        Arrays.sort(random);
-        System.out.println("Array after sorting in ascending order : " + Arrays.toString(random));
-
+        for(j=0,j<at.length;j++)
+        {
+            if(at[i]>at[j])
+            {
+                temp=at[j];
+                at[j]=at[i];
+                at[i]=temp;
+            }
+        }
     }
+    console.log("Sorted array:");
+    for(var t=0;t<at.length;t++)
+    {
+        console.log(at[t]);
+    }
+    result=at[x-1];
+    console.log("Answer is:"+result);
 }
+c1(a,4);
