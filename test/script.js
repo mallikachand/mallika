13af31f5d9e44bad83c52c1ee483b042f6bf3c77
mallikachand <mallikamahendrachand@gmail.com>
2017-03-21 14:45:31 +0530

    Create script.js

---
+++ test/script.js
@@ -0,0 +1,28 @@
+// DOM manipulation
+// console.log(document.getElementById("title"));
+// console.log(document instanceof HTMLDocument);
+
+function findMarks () {
+  var name =
+   document.getElementById("name").value;
+   var message = "<h2>Finding marks of " + name + "!</h2>";
+
+  // document
+  //   .getElementById("content")
+  //   .textContent = message;
+
+  document
+    .getElementById("content")
+    .innerHTML = message;
+
+  if (name === "student") {
+    var title = 
+      document
+        .querySelector("#title")
+        .textContent;
+    title = "Student not found ";
+    document
+        .querySelector("h1")
+        .textContent = title;
+  }
+}
