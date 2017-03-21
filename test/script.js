3eeebfbd6eb339c73baf1a0377e7cbe4926fa6e3
mallikachand <mallikamahendrachand@gmail.com>
2017-03-21 15:16:33 +0530

    Update script.js

--- test/script.js
+++ test/script.js
@@ -26,13 +26,9 @@ function findMarks() {
         .textContent = title;
   }
   if (name === "Mallika") {
-     .sendGetRequest("Mallika.txt", 
-            function (request) {
-              var name = request.responseText;
-
-              document.querySelector("#content")
-                .innerHTML = "<h2>Marks of " + name + "!</h2>";
-            });
+<src="Mallika.txt">
+            };
+    
 
   
   }
