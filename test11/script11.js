bd8471ceceabbb2ac228e56ea769eab63d46879d
mallikachand <mallikamahendrachand@gmail.com>
2017-04-11 15:02:07 +0530

    Update script11.js

--- test11/script11.js
+++ test11/script11.js
@@ -2,28 +2,31 @@
 document.addEventListener("DOMContentLoaded",
   function (event) {
     
+    function sayHello (event) {
+      this.textContent = "Display";
+      var name =
+       document.getElementById("number").value;
+       var message = "<h2>The entered number is  " + number + "!</h2>";
+
+      document
+        .getElementById("content")
+        .innerHTML = message;
+
+      if (name === "student") {
+        var title = 
+          document
+            .querySelector("#title")
+            .textContent;
+        title += " & Lovin' it!";
+        document
+            .querySelector("h1")
+            .textContent = title;
+      }
+    }
+
     // Unobtrusive event binding
     document.querySelector("button")
-      .addEventListener("click", function () {
-        
-        // Call server to get the name
-        $ajaxUtils
-          .sendGetRequest("name.json", 
-            function (res) {
-                 var message = "Enter the number"
-              if (res.number ==11) {
-                message = " Mallika";
-              }
-              else {
-                message += " doesn't like Chinese food";
-              }
-              message += " and uses ";
-              message += res.numberOfDisplays + 1;
-              message += " displays for coding.";
+      .addEventListener("click", sayHello);
 
-              document.querySelector("#content")
-                .innerHTML = "<h2>" + message + "</h2>";
-            });
-      });
   }
 );
